a546206a0f43370fe2953273c8c7ba48397833fe
Tom Whitwell <tom@whi.tw>
2021-11-23 10:47:44 +0000

    wip1

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def index_doc():
     es.indices.refresh(index="test-index")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT"))
